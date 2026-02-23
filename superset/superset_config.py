2001d38857234939db4871184c0f24dbb88d51f3
Bharath <bharathkumarkuppala@gmail.com>
2026-02-23 23:12:25 +0530

    change in superset

--- superset/superset_config.py
+++ superset/superset_config.py
@@ -2,15 +2,18 @@ import os
 
 SECRET_KEY = os.environ.get("SUPERSET_SECRET_KEY", "supersecret")
 
-# If you're accessing Superset via a public IP + no proxy:
-ENABLE_PROXY_FIX = True
-
-# Make cookies work reliably over plain HTTP (lab/dev only)
+# You're on plain HTTP over public IP -> don't mark cookies Secure
 SESSION_COOKIE_SECURE = False
+
+# Safe default for HTTP; avoids cookies being dropped
 SESSION_COOKIE_SAMESITE = "Lax"
 
-# Helps with CSRF in some setups
-WTF_CSRF_TIME_LIMIT = None
+# Helps if you're behind any proxy/NAT (common on EC2/public IP)
+ENABLE_PROXY_FIX = True
 
-# Optional: if you face weird logout due to caching
-SESSION_REFRESH_EACH_REQUEST = True
+# Make Superset generate links as http (prevents scheme confusion)
+PREFERRED_URL_SCHEME = "http"
+WEBSERVER_BASEURL = os.environ.get("WEBSERVER_BASEURL", "http://3.239.82.216:8088")
+
+# Reduce “token expired” flakiness in dev
+WTF_CSRF_TIME_LIMIT = None
